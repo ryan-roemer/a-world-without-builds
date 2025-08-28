1cea155404a01a8c1f6ee794d1e47d3680c0ef3d
Ryan Roemer <ryan@loose-bits.com>
2025-08-28 11:19:50 -0700

    Refactor stuff

---
+++ deck/media.js
@@ -0,0 +1,6 @@
+export const logos = {
+  nearform: `<img
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTODiKxPSWQzaep57CVW9j3x3n4iIlZkZLOZA&s"
+    style="width: 0.9em; height: 0.9em; margin-bottom: -0.05em"
+  />`.replace(/\n/g, ""),
+};
