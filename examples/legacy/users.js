f472d3ae1e83a288a29afb7a1ebf6ac7c939a740
Ryan Roemer <ryan@loose-bits.com>
2025-08-28 15:29:09 -0700

    Update legacy example

--- examples/legacy/users.js
+++ examples/legacy/users.js
@@ -1,4 +1,4 @@
-// Add users array to global scope (`this` or `globalThis`)
+// ⚠️ Have to add properties to global `this`
 this.users = [
   { name: "Alice", age: 25 },
   { name: "Bob", age: 30 },
