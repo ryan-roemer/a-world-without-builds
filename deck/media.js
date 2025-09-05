0beb4e93cce51b1e267d8127ec9ae9f4967534c5
Ryan Roemer <ryan@loose-bits.com>
2025-09-05 13:55:41 -0700

    Notes

--- deck/media.js
+++ deck/media.js
@@ -4,3 +4,7 @@ export const logos = {
     style="width: 0.9em; height: 0.9em; margin-bottom: -0.05em"
   />`.replace(/\n/g, ""),
 };
+
+export const backgrounds = {
+  seattle: "https://images.unsplash.com/photo-1589481169991-40ee02888551?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+};
