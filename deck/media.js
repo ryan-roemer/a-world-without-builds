bfcc5188c07e750ade4c69aaefc53e7872960654
Ryan Roemer <ryan@loose-bits.com>
2025-09-17 14:07:54 -0700

    Finalish tweaks

--- deck/media.js
+++ deck/media.js
@@ -32,4 +32,10 @@ export const backgrounds = {
   floppies: `https://images.unsplash.com/photo-1550221927-f7e52256370b?${UNSPLASH_QUERY}`,
   darkSand: `https://images.unsplash.com/photo-1533134486753-c833f0ed4866?${UNSPLASH_QUERY}`,
   skyscrapers: `https://images.unsplash.com/photo-1449157291145-7efd050a4d0e?${UNSPLASH_QUERY}`,
+  mopop: `https://images.unsplash.com/photo-1508858648555-ba5da0be9511?${UNSPLASH_QUERY}`,
+  fireworks: `https://images.unsplash.com/photo-1498931299472-f7a63a5a1cfa?${UNSPLASH_QUERY}`,
+  fire: `https://images.unsplash.com/photo-1517594422361-5eeb8ae275a9?${UNSPLASH_QUERY}`,
+  nuclear: `https://images.unsplash.com/photo-1630142895963-6996ae6b3a5b?${UNSPLASH_QUERY}`,
+  earth: `https://images.unsplash.com/photo-1534996858221-380b92700493?${UNSPLASH_QUERY}`,
+  boxingGloves: `https://images.unsplash.com/photo-1583473848882-f9a5bc7fd2ee?${UNSPLASH_QUERY}`,
 };
