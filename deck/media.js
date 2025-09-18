35c7147cc1f902c3ad6b85747283ebb8a3a45774
Ryan Roemer <ryan@loose-bits.com>
2025-09-18 11:35:29 -0700

    Add QR code

--- deck/media.js
+++ deck/media.js
@@ -20,6 +20,10 @@ export const logos = Object.fromEntries(
   ]),
 );
 
+export const images = {
+  qrCode: "/images/qr-code.png",
+};
+
 const UNSPLASH_QUERY =
   "q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
